5f2f41da4a350fc8ad65839aae7ed9c497b41b26
Duaa Azhar <duaaazhar03@gmail.com>
2023-11-29 14:01:43 +0500

    Tailwind css setup and implementation, A react Interview question related to counter project

--- 02couter/src/App.jsx
+++ 02couter/src/App.jsx
@@ -8,6 +8,20 @@ function App() {
   if(counter+1 <= 20){
   counter += 1;
   setCounter(counter);
+  // this will not increase the value by 4 bcz in react we got customization
+  // in which it updates the UI in with bunch of updates, so it will analyze the same thing is happening
+  // so it will perform it only once
+  // setCounter(counter +1);
+  // setCounter(counter +1);
+  // setCounter(counter+1);
+
+  // so for its solution, we get a hidden callback function in useState setter, 
+  // callback function will perform after the update happens 
+  setCounter(counter => counter +1);
+  setCounter(counter => counter +1);
+  setCounter(counter => counter +1);
+  setCounter(counter => counter +1);
+  
   }
   
  }
